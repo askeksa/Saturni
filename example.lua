31816e381448ff1896a21169f22762dff8ae1424
Maksim Lin <maks@manichord.com>
2023-03-23 15:24:23 +1100

    using uniforms which come from lua

--- example.lua
+++ example.lua
@@ -1,10 +1,18 @@
 title = "Minimal Test"
 
+framerate = 60
 music = "test.wav"
 shader = "helloworld.glsl"
 
 width = 640
 height = 480
+
+-- a global getter for `u_time` which is what we have as a uniform
+-- in our shader glsl to get the shader playback time in sec 
+setmetatable(_G, {__index = function (table, key)
+   if key == "u_time" then return (i / 60)  end
+  end
+})
 
 i = 0
 function frame()
--- helloworld.glsl
+++ helloworld.glsl
@@ -1,11 +1,18 @@
-// ref: https://thebookofshaders.com/02/
+// learn about uniforms
+// ref: https://thebookofshaders.com/03/
 
 #ifdef GL_ES
 precision mediump float;
 #endif
 
-uniform float u_time;
+uniform vec2 u_resolution;  // Canvas size (width,height)
+uniform vec2 u_mouse;       // mouse position in screen pixels
+uniform float u_time;       // Time in seconds since load
 
 void main() {
-	gl_FragColor = vec4(0.318,0.373,1.000,1.000);
+    float r = abs(sin(u_time * 0.8));
+    float g = abs(sin(u_time * 1.5));
+    float b = abs(sin(u_time * 0.3));
+
+	gl_FragColor = vec4(r,g,b,1.0);
 }
